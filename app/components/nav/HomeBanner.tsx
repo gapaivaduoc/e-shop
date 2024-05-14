7d1781da502d1959b91f805978a4f5dc5f37219f
Gabriel Paiva <129997355+gapaivaduoc@users.noreply.github.com>
2024-05-14 16:19:20 -0400

    banner terminado, completar el listado de productos 1:13:32

--- app/components/nav/HomeBanner.tsx
+++ app/components/nav/HomeBanner.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 const HomeBanner = () => {
     return ( 
-        <div>
-            <div>
-                <div>57:38</div>
-                <div></div>
+        <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
+            <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row item-center justify-evenly">
+                <div className="md-8 md:mb-0 text-center">
+                    <h1 className="text-4x1 md:text-6x1 font-bold text-white mb-4">Winter Sale</h1>
+                    <p className="text-lg md:text-x1 text-white md-2">Selected items</p>
+                    <p className="text-2x1 md:text-5x1 text-yellow-400 font-bold">GET 60% OFF</p>
+                </div>
+                <div className="w-1/3 relative aspect-video">
+                    <Image
+                        src='/banner-image.png'
+                        fill
+                        alt="Banner Image"
+                        className="object-contain"/>
+                </div>
             </div>
         </div>
      );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+import { products } from "@/utils/products";
+import Container from "./components/nav/Container";
+import HomeBanner from "./components/nav/HomeBanner";
+import { truncateText } from "@/utils/truncateText";
 
 export default function Home() {
   return (
-  <div>E-Shop</div>
-  )
+  <div className="p-8">
+    <Container>
+      <div>
+        <HomeBanner/>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2x1:grid-cols-6 grap:8">
+        1:13:32
+        {products.map((product: any)=>{
+          // eslint-disable-next-line react/jsx-key
+          return <div>
+            {truncateText(product.name) }
+          </div>;
+        })}
+      </div>
+    </Container>
+  </div>
+  );
 }
